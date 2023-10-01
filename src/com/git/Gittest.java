7ee228a3bfbe13173c9c7908436682e35fcfaf6e
liujun <1820159256@qq.com>
2023-10-01 10:51:54 +0800

    second commit

--- src/com/git/Gittest.java
+++ src/com/git/Gittest.java
@@ -7,6 +7,7 @@ package com.git;
 public class Gittest {
     public static void main(String[] args) {
         System.out.println("Hello git!");
+        System.out.println("hello git2!");
     }
 
 }
