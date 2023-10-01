b9c5d46622704b00105bf0eb6005ff5b257c57c0
liujun <1820159256@qq.com>
2023-10-01 10:45:53 +0800

    first commit

---
+++ src/com/git/Gittest.java
@@ -0,0 +1,12 @@
+package com.git;
+
+/**
+ * @author LiuJun
+ * @create 2023-10-01-10:39
+ */
+public class Gittest {
+    public static void main(String[] args) {
+        System.out.println("Hello git!");
+    }
+
+}
