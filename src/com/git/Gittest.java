f3d90e6e937870b2b524e8f65323ccdedc706a2d
liujun <1820159256@qq.com>
2023-10-01 10:54:23 +0800

    third commit

--- src/com/git/Gittest.java
+++ src/com/git/Gittest.java
@@ -8,6 +8,7 @@ public class Gittest {
     public static void main(String[] args) {
         System.out.println("Hello git!");
         System.out.println("hello git2!");
+        System.out.println("hello git3!");
     }
 
 }
